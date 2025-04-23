05c16e8c64465f415f14793ddfc192d5cf99900e
Avinash Patel <avipatel30092004@gmail.com>
2025-04-23 22:29:10 +0530

    create database and tables

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -1,4 +1,11 @@
-import { DB_NAME } from "../constants.js";
+import {
+  DB_NAME,
+  USER_TABLE,
+  SONG_TABLE,
+  PLAYLIST_TABLE,
+  PLAYLIST_SONG_TABLE,
+  ALBUM_TABLE,
+} from "../constants.js";
 import mysql from "mysql2/promise";
 
 let db = null; // Initialize db variable
@@ -11,8 +18,19 @@ const Connection = async () => {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: DB_NAME,
+        // database: DB_NAME,
       });
+
+      // Create the database if it doesn't exist
+      await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
+      await db.query(`USE ${DB_NAME}`); // Use the created database
+      await db.query(USER_TABLE); // Create the user table
+      await db.query(SONG_TABLE); // Create the song table
+      await db.query(PLAYLIST_TABLE); // Create the playlist table
+      await db.query(PLAYLIST_SONG_TABLE); // Create the playlist song table
+      await db.query(ALBUM_TABLE); // Create the album table
+
+      console.log("Database and tables created successfully");
       console.log("Connected to the database successfully to " + DB_NAME);
     }
     return db; // Return the db connection
